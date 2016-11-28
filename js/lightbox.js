8d4c86f0851cb268cd9a412efcca3041f3e756a3
AntonEmery <antonemery@gmail.com>
2016-11-28 12:03:00 -0800

    improved lightbox and fonts

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,14 +1,22 @@
 (function() {
-  function lightbox() {
+  function removeLightbox() {
+    $('.overlay').remove();
+    $('.lightbox-content').remove();
+  };
+  function close() {
     $('.lightbox-content p').click(function () {
-      $('.overlay').remove();
-      $('.lightbox-content').remove();
+      removeLightbox();
       });
-  }
+  };
   $('.lightbox').click(function () {
     var imgSrc = $(this).attr('src');
     var fileName = imgSrc.split('/').pop();
-    $('body').append('<div class="overlay"></div><div class="lightbox-content"><img src="img/full-size/' + fileName + '"><p>Close</p></div>');
-    lightbox();
+    $('body').append('<div class="overlay"></div><div class="lightbox-content"><img src="img/full-size/' + fileName + '"><p>Close or Esc key</p></div>');
+    close();
   });
+  $(document).keyup(function(e) {
+      if (e.keyCode == 27) {
+        removeLightbox();
+      }
+    });
 }());
